ed878d937db45806282bae29e29add7c2757d351
archturion64 <archturion64@example.com>
2023-12-29 15:19:55 +0100

    fix direct navigation reporting to lazy loaded module

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, inject } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
 import { ThemeSwitcherComponent } from '../theme-switcher/theme-switcher.component';
 import { CommonModule } from '@angular/common';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -9,16 +10,19 @@ import { CommonModule } from '@angular/common';
   imports: [RouterModule, ThemeSwitcherComponent, CommonModule],
   template: `
   <div class="navbar grid grid-cols-6 gap-4 bg-primary rounded-xl">
-    <a class="btn btn-ghost text-xl col-span-1" 
-      [ngClass]="router.url == '/' ? '!bg-secondary' : '' "  
+    @if(routerUrl$ | async; as routerUrl) {
+      <a class="btn btn-ghost text-xl col-span-1" 
+      [ngClass]="routerUrl == '/' ? '!bg-secondary' : '' "  
       [routerLink] = "['/']">
       Home
     </a>
     <a class="btn btn-ghost text-xl col-span-1  " 
-      [ngClass]="router.url == '/skills' ? '!bg-secondary' : '' "  
+      [ngClass]="routerUrl == '/skills' ? '!bg-secondary' : '' "  
       [routerLink] = "['/skills']">
       Skills
     </a>
+    }
+
     <app-theme-switcher class="col-end-7 col-span-1" [useDarkMode]="useDarkMode" (useDarkModeChange)="useDarkModeChange.emit($event)"></app-theme-switcher>
   </div>
   `,
@@ -26,7 +30,15 @@ import { CommonModule } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent {
-  router = inject(Router);
+
+  // workaround for wrong reported url when direct navigation to lazy loaded module
+  activatedRoute = inject(ActivatedRoute);
+  routerUrl$ = inject(Router).events.pipe(
+    filter(val => val instanceof NavigationEnd),
+    /* @ts-ignore  */ 
+    map((_) => this.activatedRoute._routerState.snapshot.url)
+  );
+  
 
   @Input({required: true}) useDarkMode!: boolean;
   @Output() useDarkModeChange = new EventEmitter<boolean>();
