70442be74a5cbc7ad570fa844c6b4d013ac80286
archturion64 <archturion64@gmail.com>
2024-01-06 00:27:46 +0100

    add routing to getting started

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,19 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   template: `
     <div class="hero bg-base-200 my-20 rounded-xl">
       <div class="hero-content text-center">
         <div class="max-w-md">
           <h1 class="text-5xl font-bold m-5">Greetings, Bruh!</h1>
-          <div>Join me on a journey of knoledge exploration, at the end of which you will recieve the sweet sweet Dopamine rush from the Dunning-Kruger effect.</div>
+          <div>Join me on a journey of knowledge exploration, at the end of which you will recieve the sweet sweet Dopamine rush from the Dunning-Kruger effect.</div>
           <br/>
           <div>You can find all open learning objectives in the milestones tab.</div>
           <div>You can track your progress in the progress tab.</div>
-          <button class="btn btn-primary mt-5">Get Started</button>
+          <button class="btn btn-primary mt-5" routerLink="/progress">Get Started</button>
         </div>
       </div>
     </div>
